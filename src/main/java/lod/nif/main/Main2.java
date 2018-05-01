a6733bdf72182e6a7a869412284b146b1372946c
ragnarok85 <jnht2718@hotmail.com>
2018-05-01 11:29:16 -0500

    ....

--- src/main/java/lod/nif/main/Main2.java
+++ src/main/java/lod/nif/main/Main2.java
@@ -32,6 +32,7 @@ public class Main2 {
 	 * args[1] - output folder
 	 * args[2] - outputReports
 	 * args[3] - report name - the same as the BZ2
+	 * args[4] - lang
 	 * 
 	 */
 
@@ -47,6 +48,7 @@ public class Main2 {
 		String outputFolder = args[1];
 		String outputReports = args[2];
 		String reportName = args[3];
+		String lang = args[4];
 
 
 		// System.out.println("Creating BufferedReader from BZ2 files");
@@ -55,10 +57,9 @@ public class Main2 {
 		// System.out.println("Loading the list of articles from context, page and
 		// link");
 		String reportData = "";
-		String articlesData = "";
 
 		logger.info("Extracting triples");
-		 main.createTempFiles(br, outputFolder);
+		 main.createTempFiles(br, outputFolder, lang);
 		
 		long endTime = System.currentTimeMillis() - initialTime;
 		String timeElapsed = String.format("TOTAL TIME = %d min, %d sec", TimeUnit.MILLISECONDS.toMinutes(endTime),
@@ -66,25 +67,8 @@ public class Main2 {
 						- TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(endTime)));
 		System.out.println(timeElapsed);
 		reportData += "\nTIME ELAPSED\t" + timeElapsed + "\n";
-		main.writeReport(outputReports, reportName+"-ListProcessedArticles.tsv", articlesData);
-		main.writeReport(outputReports, reportName+"-reports.tsv", reportData);
-		main.extractArticlesProcessed(outputReports, reportName);
+		main.writeReport(outputReports, reportName+"-timeElapsed.tsv", reportData);
 	}
-
-//	public List<String> readListFromFile(File pathList) {
-//		List<String> list = new ArrayList<String>();
-//		try (BufferedReader br = new BufferedReader(
-//				new InputStreamReader(new FileInputStream(pathList), StandardCharsets.UTF_8))) {
-//			String line;
-//			while ((line = br.readLine()) != null) {
-//				list.add(line);
-//			}
-//			br.close();
-//		} catch (IOException e) {
-//
-//		}
-//		return list;
-//	}
 
 	public BufferedReader getBufferedReaderForCompressedFile(String fileIn)
 			throws FileNotFoundException, CompressorException {
@@ -95,24 +79,21 @@ public class Main2 {
 		return br2;
 	}
 
-	Set<String> setArticles = new HashSet<String>();
 	int brCounter = 0;
 
-	public void createTempFiles(BufferedReader br, String outputFolder)
+	public void createTempFiles(BufferedReader br, String outputFolder, String lang)
 			throws IOException, DecoderException, NoSuchAlgorithmException {
 		boolean fin = false;
 		LOD lod = new LOD();
 		int counter = 0;
 		while (!fin) {
-			
 			String article = "";
 			List<String> lines = new ArrayList<String>();
 			fin = extractArticleLines(br, lines);
 			if (lines.size() > 0) {
 				counter += lines.size();
 				logger.info(counter);
-				article = generateName(lines.get(0));
-				setArticles.add(article);
+				article = generateName(lines.get(0), lang);
 				lod.lodFile(article, outputFolder, lines);
 			} else {
 				logger.info("\tarticle = " + article + " num lines = " + lines.size());
@@ -132,6 +113,7 @@ public class Main2 {
 		} else {
 			line = last;
 			article = last.split("\\?dbpv")[0];
+			
 		}
 		
 		while (true) {
@@ -139,8 +121,6 @@ public class Main2 {
 				fin = true;
 				break;
 			}
-				
-				
 			if (!line.startsWith("<")) {
 				line = br.readLine();
 				continue;
@@ -156,25 +136,14 @@ public class Main2 {
 				break;
 			}
 		}
-
 		return fin;
 	}
 
 
-	public void extractArticlesProcessed(String reportDirectory, String reportName) {
-		String context = "";
-		for (String a : setArticles) {
-				context += a + "\n";
-		}
-		writeReport(reportDirectory, reportName, context);
-	}
-
-	public String generateName(String uri) {
+	public String generateName(String uri, String lang) {
 		uri = uri.split("\\?")[0];
-		String split[] = uri.split("/");
-		int uriLength = split.length;
-		
-		return split[uriLength-1];
+		String uriLang = uri.split(lang+"/")[1];
+		return uriLang.replace("/", "%20");
 	}
 
 	public void printLines(List<String> lines) {
@@ -196,7 +165,7 @@ public class Main2 {
 
 	int counter = 0;
 
-	public void writeTempFile(String output, List<String> lines) {
+	public void writeTempFile(String output, List<String> lines, String lang) {
 		File file = new File(output);
 		if (!file.exists()) {
 			logger.info(counter + "-" + output);
@@ -214,8 +183,8 @@ public class Main2 {
 		}
 	}
 
-	public void writeTempFileLine(String output) {
-		String outputName = generateName(last);
+	public void writeTempFileLine(String output, String lang) {
+		String outputName = generateName(last, lang);
 		String o = output + "/" + outputName+ ".nt";
 		try (PrintWriter pw = new PrintWriter(
 				new OutputStreamWriter(new FileOutputStream(o, true), StandardCharsets.UTF_8))) {

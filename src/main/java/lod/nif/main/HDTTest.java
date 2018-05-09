809d1722944cadd5964a461b681dbe07efa0c09e
ragnarok85 <jnht2718@hotmail.com>
2018-05-08 19:23:35 -0500

    ...

--- src/main/java/lod/nif/main/HDTTest.java
+++ src/main/java/lod/nif/main/HDTTest.java
@@ -1,36 +1,69 @@
 package lod.nif.main;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
-import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
+import org.rdfhdt.hdt.enums.RDFNotation;
 import org.rdfhdt.hdt.exceptions.ParserException;
 import org.rdfhdt.hdt.hdt.HDT;
 import org.rdfhdt.hdt.hdt.HDTManager;
-import org.rdfhdt.hdt.hdt.HDTManagerImpl;
 import org.rdfhdt.hdt.options.HDTSpecification;
 import org.rdfhdt.hdt.rdf.parsers.JenaModelIterator;
-import org.rdfhdt.hdt.enums.RDFNotation;
 
 public class HDTTest  {
 
 	public static void main(String[] args) throws IOException, ParserException, CompressorException {
 		// TODO Auto-generated method stub
 	//	JenaModelIterator jmi = new JenaModelIterator(Model model);
+//		List<String> lines = new ArrayList<String>();
+//		createOneHDT(lines, "hdtoutput.hdt");
+		
+		HDTTest main  = new HDTTest();
+		FileInputStream fin = new FileInputStream("sample.ttl.bz2");
+		BufferedInputStream bis = new BufferedInputStream(fin);
+		CompressorInputStream input = new CompressorStreamFactory().createCompressorInputStream(bis);
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(input));
+		
+		String line = "";
 		List<String> lines = new ArrayList<String>();
-		createOneHDT(lines, "hdtoutput.hdt");
-
+		while((line = br2.readLine()) != null){
+			System.out.println(line);
+			lines.add(line);
+		}
+		main.parseFileGZ("sample2.ttl.bz2", lines);
+	}
+	
+	public void parseFileGZ(String outputPath, List<String> lines) throws IOException{
+		//OutputStream os = Files.newOutputStream(Paths.get(outputPath));
+		OutputStream os = new FileOutputStream(outputPath,true);
+		BufferedOutputStream bos = new BufferedOutputStream(os);
+		BZip2CompressorOutputStream outputStream = new BZip2CompressorOutputStream(bos);
+//		PrintWriter pw = new PrintWriter(outputStream);
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(outputStream,Charset.forName("UTF-8")));
+		for(String line : lines){
+			pw.write(line + "\n");
+		}
+		pw.close();
 	}
 	
 	public static void createHDT(Model model, String hdtOutput, String baseURI) throws IOException, ParserException{
